7b953370fee64d0c223adb7eb7b7e16a3c2d6a37
DataVan-Ansel_Chen <ansel_chen@datavan.com.tw>
2023-03-15 16:01:46 +0800

    'first'

--- models.py
+++ models.py
@@ -2,34 +2,39 @@ import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
 
-class Book(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    title: str = Field(...)
-    author: str = Field(...)
-    synopsis: str = Field(...)
+
+class Member(BaseModel):
+    account: str = Field(max_length=16,
+                         min_length=8, description='最少輸入8個字元,最多16個字元')
+    password: str = Field(...)
+    name: str = Field(...)
+    phone: str = Field(...)
+    '''
+    Field(...)是必填欄位,Field（None） 是可選填
+    Field(None,title="The description of the item",max_length=10,alias='我是替代字')
+
+    '''
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
-                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "title": "Don Quixote",
-                "author": "Miguel de Cervantes",
-                "synopsis": "..."
+                "account": "testtest123",
+                "password": "testpassword",
+                "name": "Ansel",
+                "phone": "0912345678"
             }
         }
 
 
-class BookUpdate(BaseModel):
-    title: Optional[str]
-    author: Optional[str]
-    synopsis: Optional[str]
+class MemberUpdate(BaseModel):
+    name: Optional[str]
+    phone: Optional[str]
 
     class Config:
         schema_extra = {
             "example": {
-                "title": "Don Quixote",
-                "author": "Miguel de Cervantes",
-                "synopsis": "Don Quixote is a Spanish novel by Miguel de Cervantes..."
+                "name": "Zoe",
+                "phone": "0900123321"
             }
         }
--- routes.py
+++ routes.py
@@ -2,61 +2,76 @@ from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from typing import List
 
-from models import Book, BookUpdate
+from models import Member, MemberUpdate
 
 router = APIRouter()
 
-@router.post("/", response_description="Create a new book", status_code=status.HTTP_201_CREATED, response_model=Book)
-def create_book(request: Request, book: Book = Body(...)):
-    book = jsonable_encoder(book)
-    new_book = request.app.database["books"].insert_one(book)
-    created_book = request.app.database["books"].find_one(
-        {"_id": new_book.inserted_id}
+
+@router.post("/", response_description="Create a new account", status_code=status.HTTP_201_CREATED, response_model=Member)
+def create_member(request: Request, member: Member = Body(...)):
+    # 檢查帳號是否已存在
+    existing_member = request.app.database["member_data"].find_one(
+        {"account": member.account}
+    )
+    if existing_member:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Account already exists")
+
+    # 新增會員
+    member = jsonable_encoder(member)
+    new_member = request.app.database["member_data"].insert_one(member)
+    created_member = request.app.database["member_data"].find_one(
+        {"_id": new_member.inserted_id}
     )
 
-    return created_book
+    return created_member
 
 
-@router.get("/", response_description="List all books", response_model=List[Book])
-def list_books(request: Request):
-    books = list(request.app.database["books"].find(limit=100))
+@router.get("/", response_description="List all member", response_model=List[Member])
+def list_memberships(request: Request):
+    books = list(request.app.database["member_data"].find(limit=100))
     return books
 
 
-@router.get("/{id}", response_description="Get a single book by id", response_model=Book)
-def find_book(id: str, request: Request):
-    if (book := request.app.database["books"].find_one({"_id": id})) is not None:
-        return book
+@router.get("/{account}", response_description="Get member by id", response_model=Member)
+def find_member(account: str, request: Request):
+    if (member := request.app.database["member_data"].find_one({"account": account})) is not None:
+        return member
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Member with account {account} not found")
 
 
-@router.put("/{id}", response_description="Update a book", response_model=Book)
-def update_book(id: str, request: Request, book: BookUpdate = Body(...)):
-    book = {k: v for k, v in book.dict().items() if v is not None}
+@router.patch("/{account}", response_description="Update member info", response_model=Member)
+def update_member(account: str, request: Request, member: MemberUpdate = Body(...)):
+    member = {k: v for k, v in member.dict().items() if v is not None}
 
-    if len(book) >= 1:
-        update_result = request.app.database["books"].update_one(
-            {"_id": id}, {"$set": book}
+    if len(member) >= 1:
+        update_result = request.app.database["member_data"].update_one(
+            {"account": account}, {"$set": member}
         )
 
         if update_result.modified_count == 0:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Member with ID {account} not found")
 
     if (
-        existing_book := request.app.database["books"].find_one({"_id": id})
+        existing_book := request.app.database["member_data"].find_one({"account": account})
     ) is not None:
         return existing_book
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Member with ID {account} not found")
 
 
-@router.delete("/{id}", response_description="Delete a book")
-def delete_book(id: str, request: Request, response: Response):
-    delete_result = request.app.database["books"].delete_one({"_id": id})
+@router.delete("/{account}", response_description="Delete account")
+def delete_member(account: str, request: Request, response: Response):
+    delete_result = request.app.database["member_data"].delete_one(
+        {"account": account})
 
     if delete_result.deleted_count == 1:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Member with ID {account} not found")
